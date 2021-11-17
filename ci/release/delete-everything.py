f5bf457a52f6179c4036ff906d2a7d694d77fce4
gltile <80788438+gltile-two-electric-boogaloo@users.noreply.github.com>
2021-11-17 15:46:54 +0000

    stop deleting everything

--- ci/release/delete-everything.py
+++ ci/release/delete-everything.py
@@ -2,12 +2,12 @@ import pathlib
 import os
 import shutil
 
-WORKSPACE = pathlib.Path(os.environ['GITHUB_WORKSPACE'])
+WORKSPACE = pathlib.Path(os.environ['GITHUB_WORKSPACE']).absolute()
 
 
 def main():
     for file in WORKSPACE.iterdir():
-        if WORKSPACE / 'website' in file.parents or WORKSPACE / '.git' in file.parents:
+        if file == WORKSPACE / '.git' or file == WORKSPACE / 'website':
             continue
 
         print(f"Removing {file.absolute()}")
